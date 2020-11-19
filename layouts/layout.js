dc7e8d5c6b3341f992d86b104df54534b37b401b
yatharth <yatharthverma070@gmail.com>
2020-11-19 22:42:05 +0530

    Add plus icon in navbar

--- layouts/layout.js
+++ layouts/layout.js
@@ -3,6 +3,8 @@ import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstr
 import {Link} from 'next/link';
 import {useAuth} from '../contexts/AuthProvider';
 import {useRouter} from 'next/router';
+import {Fab} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 
 const Layout = ({children}) => {
     
@@ -17,23 +19,23 @@ const Layout = ({children}) => {
 
     return (
         <div>            
-            <Navbar collapseOnSelect className = "nav-color-custom" expand="lg"  variant = "dark">
+            <Navbar collapseOnSelect className = "nav-color-custom" expand="lg"  variant = "light" sticky = "top">
                 <Navbar.Brand style={{paddingTop : ".1025rem"}}>DevBlogs</Navbar.Brand>
                 <Navbar.Toggle />
             <Navbar.Collapse>
             <Nav className = "mr-auto">
-                {currUser ? <Nav.Link href = "/dashboard">Dashboard</Nav.Link> : <Nav.Link as={Link} href="/">Home</Nav.Link>}
+                {currUser ? <Nav.Link href = "/dashboard">Dashboard</Nav.Link> : <Nav.Link href="/">Home</Nav.Link>}
                 <Nav.Link href="/contribute">Contribute</Nav.Link>
-                <Nav.Link href = "/posts/create">Create Blog</Nav.Link>
                 {currUser ? <Button variant = "light" className = "ml-1" onClick = {handleLogout}>Logout</Button>  : <NavDropdown title="Signup / Login">
-                    <NavDropdown.Item href = "/login">Login</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} href = "/login"><a>Login</a></NavDropdown.Item>
                     <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
                 </NavDropdown>}
              </Nav>
              
              <Form inline>
+                <Nav.Link href = "/posts/create"><a><Fab color="primary" aria-label="add" size = "small" className= "mr-2"><AddIcon /></Fab></a></Nav.Link>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-light">Search</Button>
+                <Button variant="outline-primary">Search</Button>
             </Form>
              
             </Navbar.Collapse>
