aa9bd0d626d20514035226a9b36466774ccaa953
yatharth <yatharthverma070@gmail.com>
2020-11-21 17:43:26 +0530

    Change navbar

--- layouts/layout.js
+++ layouts/layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
-import {Link} from 'next/link';
 import {useAuth} from '../contexts/AuthProvider';
 import {useRouter} from 'next/router';
 import {Fab} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import Link from 'next/link';
 
 const Layout = ({children}) => {
-    
     const {currUser, logout} = useAuth();
     const router = useRouter();
 
@@ -19,29 +18,28 @@ const Layout = ({children}) => {
 
     return (
         <div>            
-            <Navbar collapseOnSelect className = "nav-color-custom" expand="lg"  variant = "light" sticky = "top">
-                <Navbar.Brand style={{paddingTop : ".1025rem"}}>DevBlogs</Navbar.Brand>
-                <Navbar.Toggle />
-            <Navbar.Collapse>
-            <Nav className = "mr-auto">
-                {currUser ? <Nav.Link href = "/dashboard">Dashboard</Nav.Link> : <Nav.Link href="/">Home</Nav.Link>}
-                <Nav.Link href="/contribute">Contribute</Nav.Link>
-                {currUser ? <Button variant = "light" className = "ml-1" onClick = {handleLogout}>Logout</Button>  : <NavDropdown title="Signup / Login">
-                    <NavDropdown.Item as={Link} href = "/login"><a>Login</a></NavDropdown.Item>
-                    <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
-                </NavDropdown>}
-             </Nav>
-             
-             <Form inline>
-                <Nav.Link href = "/posts/create"><a><Fab color="primary" aria-label="add" size = "small" className= "mr-2"><AddIcon /></Fab></a></Nav.Link>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-primary">Search</Button>
-            </Form>
-             
-            </Navbar.Collapse>
-            
-            </Navbar>
+            <div style={{width: "90%", margin: "0 auto"}}>
+                <Navbar collapseOnSelect className = "nav-color-custom" expand="lg"  variant = "light" sticky = "top">
+                    <Navbar.Brand style={{paddingTop : ".1025rem", color: "black", "fontWeight" : "bold"}}><Link href = "/"><a>DevBlogs</a></Link></Navbar.Brand>
+                    <Navbar.Toggle />
+                <Navbar.Collapse>
 
+                <Nav className = "mr-auto">
+                    <div style={{width: "65px", height: "31px", backgroundColor : "#162353", borderRadius : "100px", alignItems:"center", padding:"3.5px"}}>
+                        <div style={{width:"24px", height: "24px", borderRadius : "50%", backgroundColor:"white"}}></div>
+                    </div>    
+                </Nav>
+
+                <Form inline>
+                    <Nav.Link as={Link} href = "/posts/create" className = "mr-3"><a><Fab color="dark" aria-label="add" size = "small" className= "mr-3"><AddIcon /></Fab></a></Nav.Link>
+                    {currUser ? <Button style={{backgrountColor : "#162353"}} onClick = {handleLogout}>Logout</Button> : <div><Button className = "mr-3" style={{backgroundColor: "#162353", border:"1px solid #162353"}}>SignIn</Button>
+                    <Button variant = "dark">Create account</Button></div>}
+                </Form>
+                
+                </Navbar.Collapse>
+                
+                </Navbar>
+            </div>
             {children}
         </div>
        
