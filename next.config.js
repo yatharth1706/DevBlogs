c24d0f2b87c6a9f071c2ed8f06b7806ae3cc0226
yatharth <yatharthverma070@gmail.com>
2021-09-26 13:09:11 +0530

    fix issue

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseUrl: process.env.databaseUrl,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+  },
+};
--- pages/login.js
+++ pages/login.js
@@ -24,18 +24,17 @@ const Login = () => {
     animationOut: ["animate__animated animate__fadeOut"], // `animate.css v4` classes
   };
 
-  const errorNotification = {
-    title: error,
-    message: "",
-    type: "danger",
-    insert: "top",
-    container: "top-right",
-    animationIn: ["animate__animated animate__fadeIn"], // `animate.css v4` classes
-    animationOut: ["animate__animated animate__fadeOut"], // `animate.css v4` classes
-  };
-
   useEffect(() => {
     if (error.trim() !== "") {
+      let errorNotification = {
+        title: error,
+        message: error,
+        type: "danger",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated animate__fadeIn"], // `animate.css v4` classes
+        animationOut: ["animate__animated animate__fadeOut"], // `animate.css v4` classes
+      };
       store.addNotification({
         ...errorNotification,
         container: "top-right",
