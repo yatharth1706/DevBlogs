15389dcd4cb7eeeb838e1a5aacabe598eb07fe1f
yatharth <yatharthverma070@gmail.com>
2021-10-09 18:45:59 +0530

    debug issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,28 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from '../layouts/layout';
-import {AuthProvider} from '../contexts/AuthProvider';
-import ReactNotification from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css'
-import Head from 'next/head';
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "../layouts/layout";
+import { AuthProvider } from "../contexts/AuthProvider";
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <AuthProvider>
-    <Layout>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300&display=swap" rel="stylesheet" />
-      </Head>
-      <ReactNotification />      
-      <Component {...pageProps} />
-    </Layout>
-  </AuthProvider>
-  )
+    <AuthProvider>
+      <Layout>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <ReactNotification />
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
